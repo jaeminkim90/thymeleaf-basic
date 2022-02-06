f669700de01fa2c8938d65ff4a0ad892ebf0f307
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-06 18:47:10 +0900

    FEAT: 타임리프가 제공하는 주석 기능 예제 코드 구현

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -119,6 +119,13 @@ public class BasicController {
         model.addAttribute("users", list);
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+
 
     @Data
     static class User {
